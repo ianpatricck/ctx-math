ffa6cdcb09524b0857433263e2bb616ba5f7294f
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-02 20:18:01 -0300

    View result updated

--- client/js/submit.js
+++ client/js/submit.js
@@ -1,7 +1,20 @@
 var question = document.getElementById('question')
 
+function createResultMessage(result) {
+    if (document.querySelector('.result-value')) {
+        const resultValue = document.querySelector('.result-value')
+        document.querySelector('.result').removeChild(resultValue)
+    }
+
+    const resultValue = document.createElement('h3')
+    resultValue.classList.add('result-value')
+
+    document.querySelector('.result').appendChild(resultValue)
+    resultValue.innerText = String(result.result)
+}
+
 async function submit() {
-    fetch('http://localhost:8000/resolve', {
+    await fetch('http://localhost:8000/resolve', {
         method: 'POST',
         mode: 'cors',
         cache: 'no-cache',
@@ -11,5 +24,10 @@ async function submit() {
         },
         body: JSON.stringify({ message: question.value })
         
-    }).then(res => res.json())
+    })
+    .then(res => res.json())
+    .then(result => {
+        createResultMessage(result)
+    })
+    
 }
