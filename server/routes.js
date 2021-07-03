bc12a5fdc72b84cc9b5fa19a745d8670c4f3710d
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-03 20:31:47 -0300

    Fixed error message to client

--- server/routes.js
+++ server/routes.js
@@ -5,12 +5,31 @@ const SimpleOperations = require('./resolvers/SimpleOperations')
 
 var simpleOperations = new SimpleOperations()
 
+const TestOperationsStatus = (result = []) => {
+    const finalResult = result.map((operation) => {
+        return !Number(operation) && operation == false ? "It was not possible to perform the calculation" : operation
+    })
+
+    return finalResult
+}
+
 router
 
     .post('/resolve', (req, res) => {
         if (req.body.message) {
-            const basicResult = simpleOperations.basicOperators(req.body.message)
-            res.json({ result: basicResult })
+
+            /*
+             * Tests
+             * 
+             */
+
+            let basicOperations = simpleOperations.basicOperators(req.body.message)
+
+            const result = TestOperationsStatus([
+                basicOperations
+            ])
+
+            res.json({ result: result })
         }
     })
 
