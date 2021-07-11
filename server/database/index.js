b2c1170fe0d02f1bbcf46c3acec8b75ebfed8413
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-10 21:07:48 -0300

    Object of arrays database

--- server/database/index.js
+++ server/database/index.js
@@ -3,10 +3,15 @@ const dataOperationsContext = [
         'addition': [
             'mais', 
             'adicionado'
-        ]
-    },
-    {
+        ],
+
         'subtraction': [
+            'menos', 
+            'subtraído', 
+            'subtraido'
+        ],
+
+        'multiplication': [
             'menos', 
             'subtraído', 
             'subtraido'
