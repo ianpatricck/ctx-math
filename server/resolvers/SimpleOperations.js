4de74e98d965f26f3251c8388b53989cfc5f9e99
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-12 20:59:04 -0300

    loopForAddition init

--- server/resolvers/SimpleOperations.js
+++ server/resolvers/SimpleOperations.js
@@ -1,19 +1,39 @@
+const dataOperationsContext = require('../database')
+
 /**
  * Simple Operations
  * 
  */
 
+function loopForAddition(currentQuestion, keyWords) {
+    let finalFormat
+
+    keyWords.forEach((key) => {
+        if (currentQuestion.includes(key))
+            currentQuestion = currentQuestion.replace(new RegExp(key, 'gi'), '+')
+        finalFormat = currentQuestion
+    })
+
+    return finalFormat
+}
+
 class SimpleOperations {
-    basicOperators(question) {
+    basic(question) {
         try {
             return eval(question)
         } catch {
-            return false;
+            return false
         }
     }
 
-    contextedOperators() {
-        // ..
+    contextualized(question) {
+        let finalQuestion
+
+        const additionArray = dataOperationsContext.map((elements) => elements.addition)
+
+        finalQuestion = loopForAddition(question, additionArray.join(' ').split(','))
+
+        return eval(finalQuestion)
     }
 }
 
