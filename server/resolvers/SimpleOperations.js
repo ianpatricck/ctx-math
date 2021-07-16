3302a0011aefe5c0c2e6548c24f0d02161dbdcf9
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-15 23:03:58 -0300

    Operation functions

--- server/resolvers/SimpleOperations.js
+++ server/resolvers/SimpleOperations.js
@@ -17,6 +17,54 @@ function loopForAddition(currentQuestion, keyWords) {
     return finalFormat
 }
 
+function loopForSubtraction(currentQuestion, keyWords) {
+    let finalFormat
+
+    keyWords.forEach((key) => {
+        if (currentQuestion.includes(key))
+            currentQuestion = currentQuestion.replace(new RegExp(key, 'gi'), '-')
+        finalFormat = currentQuestion
+    })
+
+    return finalFormat
+}
+
+function loopForMultiplication(currentQuestion, keyWords) {
+    let finalFormat
+
+    keyWords.forEach((key) => {
+        if (currentQuestion.includes(key))
+            currentQuestion = currentQuestion.replace(new RegExp(key, 'gi'), '*')
+        finalFormat = currentQuestion
+    })
+
+    return finalFormat
+}
+
+function loopForDivision(currentQuestion, keyWords) {
+    let finalFormat
+
+    keyWords.forEach((key) => {
+        if (currentQuestion.includes(key))
+            currentQuestion = currentQuestion.replace(new RegExp(key, 'gi'), '/')
+        finalFormat = currentQuestion
+    })
+
+    return finalFormat
+}
+
+function loopForPotentiation(currentQuestion, keyWords) {
+    let finalFormat
+
+    keyWords.forEach((key) => {
+        if (currentQuestion.includes(key))
+            currentQuestion = currentQuestion.replace(new RegExp(key, 'gi'), '**')
+        finalFormat = currentQuestion
+    })
+
+    return finalFormat
+}
+
 class SimpleOperations {
     basic(question) {
         try {
@@ -30,8 +78,18 @@ class SimpleOperations {
         let finalQuestion
 
         const additionArray = dataOperationsContext.map((elements) => elements.addition)
+        const subtractionArray = dataOperationsContext.map((elements) => elements.subtraction)
+        const multiplicationArray = dataOperationsContext.map((elements) => elements.multiplication)
+        const divisionArray = dataOperationsContext.map((elements) => elements.division)
+        const potentiationArray = dataOperationsContext.map((elements) => elements.potentiation)
 
         finalQuestion = loopForAddition(question, additionArray.join(' ').split(','))
+        finalQuestion = loopForSubtraction(finalQuestion, subtractionArray.join(' ').split(','))
+        finalQuestion = loopForMultiplication(finalQuestion, multiplicationArray.join(' ').split(','))
+        finalQuestion = loopForDivision(finalQuestion, divisionArray.join(' ').split(','))
+        finalQuestion = loopForPotentiation(finalQuestion, potentiationArray.join(' ').split(','))
+        
+        console.log(finalQuestion)
 
         return eval(finalQuestion)
     }
