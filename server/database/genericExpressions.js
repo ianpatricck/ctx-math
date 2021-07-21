f93e33946c43a44c1f6eaf19b30d69ea1d00b523
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-21 11:50:39 -0300

    Generic expressions that may or may not be needed

---
+++ server/database/genericExpressions.js
@@ -0,0 +1,19 @@
+const genericQuestion = [
+    {
+        'division': [
+            'a razão entre',
+            'razão entre',
+            'a razao entre',
+            'razao entre',
+        ],
+
+        'multiplication': [
+            'o produto de',
+            'produto de',
+            'o produto entre',
+            'produto entre',
+        ]   
+    }
+]
+
+module.exports = genericQuestion
