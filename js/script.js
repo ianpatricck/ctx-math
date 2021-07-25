b56b6d8f8bd51a2e01a70cfb15378c165e72fa51
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-07-25 20:01:03 -0300

    Redirecting to other API URL

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var question = document.getElementById('question')
+
+const API_URL = '' // API para envio das questões.
+
+function createResultMessage(result) {
+    // Verifica se existe um valor já exibido na tela
+    // -> Caso exista, exclui a mensagem para evitar duplicação.
+
+    if (document.querySelector('.result-value-success')) {
+        const resultValueSuccess = document.querySelector('.result-value-success')
+        document.querySelector('.result').removeChild(resultValueSuccess)
+    }
+
+    // Verifica se existe uma mensagem de erro
+    // -> Caso exista, exclui a mensagem para evitar duplicação.
+    
+    if (document.querySelector('.result-value-danger')) {
+        const resultValueDanger = document.querySelector('.result-value-danger')
+        document.querySelector('.result').removeChild(resultValueDanger)
+    }
+
+    const resultValue = document.createElement('h3')
+
+    if (Number(result.result)) {
+        resultValue.classList.add('result-value-success')
+    } else {
+        resultValue.classList.add('result-value-danger')
+    }
+
+    document.querySelector('.result').appendChild(resultValue)
+    resultValue.innerText = String(result.result)
+}
+
+async function submit() {
+    await fetch(API_URL, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: question.value })
+
+    })
+        .then(res => res.json())
+        .then(result => {
+            createResultMessage(result)
+        })
+}
