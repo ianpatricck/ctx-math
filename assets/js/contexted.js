ccb30f1770c543ede16cf106559878adab408979
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-08-30 22:37:27 -0300

    Separate submit questions file

--- assets/js/contexted.js
+++ assets/js/contexted.js
@@ -1,7 +1,5 @@
 var question = document.getElementById('question')
 
-const API_URL = '' // API para envio das questões.
-
 function createResultMessage(result) {
     // Verifica se existe um valor já exibido na tela
     // -> Caso exista, exclui a mensagem para evitar duplicação.
@@ -29,22 +27,4 @@ function createResultMessage(result) {
 
     document.querySelector('.result').appendChild(resultValue)
     resultValue.innerText = String(result.result)
-}
-
-async function submit() {
-    await fetch(API_URL, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ message: question.value })
-
-    })
-        .then(res => res.json())
-        .then(result => {
-            createResultMessage(result)
-        })
 }
